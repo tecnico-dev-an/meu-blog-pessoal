9c4b1e1b70726b4a8dc6d51947558384f17c2b43
tecnico-dev-an <tecnico.dev.an@gmail.com>
2025-08-20 21:51:39 -0300

    commit fim do dia de trabalho

--- index.php
+++ index.php
@@ -1,11 +1,52 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meu blog pessoal</title>
-</head>
+<html lang="pt-br">
+<title>Meu Blog</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body>
-    
+
+    <header class="w3-container w3-center w3-padding-32 w3-red">
+        <h1><b>MEU BLOG PESSOAL</b></h1>
+        <p>Seja Bem-vindo. Criado por <span class="w3-tag">NOME</span></p>
+    </header>
+
+    <div class="w3-row w3-padding">
+        <div class="w3-col l8 s12">
+            <div class="w3-container w3-card-4 w3-margin-bottom">
+                <div class="w3-container w3-white">
+                    <p>Postagem...</p>
+                </div>
+            </div>
+            <div class="w3-container w3-card-4 w3-margin-bottom">
+                <div class="w3-container w3-white">
+                    <p>Outra postagem...</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="w3-col l4">
+            <div class="w3-container w3-card-4 w3-margin-bottom">
+                <div class="w3-container w3-white">
+                    <p>Sobre mim...</p>
+                </div>
+            </div>
+            <div class="w3-container w3-card-4 w3-margin-bottom">
+                <div class="w3-container w3-white">
+                    <p>Top 5 posts...</p>
+                </div>
+            </div>
+            <div class="w3-container w3-card-4 w3-margin-bottom">
+                <div class="w3-container w3-white">
+                    <p>Filtro...</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="w3-container w3-center w3-padding-32 w3-black">
+        <p>Rodapé do Blog...</p>
+    </footer>
+<?php echo "Hello World!"; ?>
 </body>
 </html>
